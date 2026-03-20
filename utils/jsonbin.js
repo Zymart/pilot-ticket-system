e40dc5b8c33af6f1054f5e0ce353bb8ddfc3079d
Zymart <144964437+Zymart@users.noreply.github.com>
2026-03-20 09:17:59 +0800

    Update jsonbin.js

--- utils/jsonbin.js
+++ utils/jsonbin.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 const JSONBIN_ROOT = 'https://api.jsonbin.io/v3';
 const MASTER_KEY = process.env.JSONBIN_MASTER_KEY;
 
@@ -9,7 +10,6 @@ class JSONBin {
         };
     }
 
-    // Create new bin
     async create(data) {
         const res = await fetch(`${JSONBIN_ROOT}/b`, {
             method: 'POST',
@@ -17,10 +17,9 @@ class JSONBin {
             body: JSON.stringify(data)
         });
         const json = await res.json();
-        return json.metadata.id; // Returns bin ID
+        return json.metadata.id;
     }
 
-    // Read bin
     async read(binId) {
         const res = await fetch(`${JSONBIN_ROOT}/b/${binId}`, {
             headers: this.headers
@@ -29,21 +28,11 @@ class JSONBin {
         return json.record;
     }
 
-    // Update bin
     async update(binId, data) {
         const res = await fetch(`${JSONBIN_ROOT}/b/${binId}`, {
             method: 'PUT',
             headers: this.headers,
             body: JSON.stringify(data)
-        });
-        return res.ok;
-    }
-
-    // Delete bin
-    async delete(binId) {
-        const res = await fetch(`${JSONBIN_ROOT}/b/${binId}`, {
-            method: 'DELETE',
-            headers: this.headers
         });
         return res.ok;
     }
